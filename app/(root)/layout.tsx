4f8cdc7b15d86dffc74f7f61d47a392f4c6214bb
Deepak Kumar <dpk.41deep@gmail.com>
2026-02-18 18:12:41 +0530

    Successfully deployed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import  {Footer}  from "@/features/home/footer";
 import  {Header}  from "@/features/home/header";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import { auth } from "@/auth-edge";
 // import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
@@ -12,25 +13,32 @@ export const metadata: Metadata = {
   },
 };
 
-export default function HomeLayout({
+export default async function HomeLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const session = await auth();
+    // const pathname = usePathname();
+
     return (
         <>
+        {session ? (
             <Header />
+        ) : (
+            null
+        )}
 
             <div
                 className={cn(
                     "absolute inset-0 -z-10",
-                    "[background-size:40px_40px]",
-                    "[background-image:linear-gradient(to_right,rgba(56,189,248,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(99,102,241,0.08)_1px,transparent_1px)]"
+                    "bg-size-[40px_40px]",
+                    "bg-[linear-gradient(to_right,rgba(56,189,248,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(99,102,241,0.08)_1px,transparent_1px)]"
                 )}
             />
 
             <div className="pointer-events-none absolute inset-0 flex items-center justify-center -z-10">
-                <div className="w-[700px] h-[700px] bg-gradient-to-r from-blue-500/20 via-teal-400/20 to-indigo-500/20 blur-[140px] rounded-full" />
+                <div className="w-[700px] h-[700px] bg-linear-to-r from-blue-500/20 via-teal-400/20 to-indigo-500/20 blur-[140px] rounded-full" />
             </div>
 
             <main className="relative z-10 w-full min-h-screen bg-gray-950">
